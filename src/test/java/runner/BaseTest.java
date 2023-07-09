a1b31515f4a51a75ab6d183ea3d99ff7eb4fde7c
almalmalm <almostkalinin@gmail.com>
2023-07-09 13:08:55 +0400

    init

---
+++ src/test/java/runner/BaseTest.java
@@ -0,0 +1,42 @@
+package runner;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class BaseTest {
+
+    private WebDriverWait wait3;
+    private WebDriver driver;
+
+    @BeforeMethod
+    protected void beforeMethod() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+//        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+        WebDriverManager.chromedriver().setup();
+
+        driver = new ChromeDriver(chromeOptions);
+    }
+
+    @AfterMethod
+    protected void afterMethod() {
+        driver.quit();
+    }
+
+    protected WebDriver getDriver() {
+        return driver;
+    }
+
+    public WebDriverWait getWait3() {
+        if(wait3 == null) {
+            wait3 = new WebDriverWait(getDriver(), Duration.ofSeconds(3));
+        }
+        return wait3;
+    }
+}
